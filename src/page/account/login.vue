739b32e6d895a05533221cb941242e77b93dd6ab
ht1131589588 <1131589588@qq.com>
2017-11-26 21:08:55 +0800

    登陆

--- src/page/account/login.vue
+++ src/page/account/login.vue
@@ -4,32 +4,39 @@
       <h3 class="login-box-title">登录</h3>
       <div class="login-box-form">
         <div class="common-mt-15">
-          <el-input v-model="username" placeholder="请输入用户名"></el-input>
+          <el-input v-model="loginForm.username" placeholder="请输入用户名"></el-input>
         </div>
         <div class="common-mt-15">
-          <el-input type="password" v-model="password" placeholder="请输入密码"></el-input>
+          <el-input type="password" v-model="loginForm.password" placeholder="请输入密码"></el-input>
         </div>
         <div class="common-mt-15">
-          <el-button type="button">登录</el-button>
+          <el-button type="primary" class="login-submit-btn" @click="submitForm">登录</el-button>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
-  import fetch from '@/config/fetch'
+  import {login} from '@/api/data'
+  import {mapActions, mapState} from 'vuex'
+
   export default {
     data() {
       return {
-        username:'',
-        password:''
+        loginForm:{
+          username:'',
+          password:''
+        }
       };
     },
     mounted(){
-     
+      
     },
     methods: {
-      
+      async submitForm() {
+        console.log(this.loginForm);
+        const res = await login({user_name: this.loginForm.username, password: this.loginForm.password})
+      }
     }
   }
 </script>
@@ -42,14 +49,15 @@
     position: relative;
     .login-box{
       position: absolute;
-      width: 320px;height: 400px;
+      width: 320px;
+      height: auto;
       background: #fff;
       border-radius: 5px;
       box-shadow: 0px 0px 10px rgba(222,222,222,.3);
       top: 50%;
       left: 50%;
       transform: translate(-50%,-50%);
-      padding: 16px;
+      padding: 16px 16px 30px;
       box-sizing: border-box;
     }
     .login-box-title{
@@ -58,6 +66,9 @@
     }
     .login-box-form{
       margin-top: 15px;
+    }
+    .login-submit-btn{
+      width: 100%;
     }
   }
 </style>
